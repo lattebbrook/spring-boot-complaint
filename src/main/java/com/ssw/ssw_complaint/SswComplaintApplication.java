d65e23b5b8d466e13f8920951a6c6e4ba38e559e
Tanawat Boonmak <tanawat.brook@icloud.com>
2023-03-03 20:38:39 +0700

    Update login system to be manually adjustable
    Update login system to be manually editable (unsafe method) for the sake of test or waiting for another system to implement.

--- src/main/java/com/ssw/ssw_complaint/SswComplaintApplication.java
+++ src/main/java/com/ssw/ssw_complaint/SswComplaintApplication.java
@@ -2,13 +2,12 @@ package com.ssw.ssw_complaint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SswComplaintApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SswComplaintApplication.class, args);
+            SpringApplication.run(SswComplaintApplication.class, args);
 	}
 
 }
